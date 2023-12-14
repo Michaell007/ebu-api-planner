deea45ccb5a0f57f27719b5b533b937f64a49650
Michael Yaps <michaelakichigmail.com>
2023-12-14 01:08:02 +0000

    add new correction

---
+++ src/models/detailsCommande/index.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+import timestampPlugin from 'mongoose-timestamp';
+import User from '../user';
+import _ from "lodash";
+
+const detailsCommandeSchema = new Schema({
+    horaire: {
+        type: String,
+        required: false
+    },
+    commandeId: {type: mongoose.Types.ObjectId, ref: "Commande"},
+    evenementId: {type: mongoose.Types.ObjectId, ref: "Evenement"}
+});
+
+detailsCommandeSchema.methods = {};
+detailsCommandeSchema.statics = {};
+detailsCommandeSchema.plugin(timestampPlugin);
+
+const model = mongoose.model("DetailsCommande", detailsCommandeSchema);
+
+export const schema = model.schema;
+export default model;
--- src/routes/commande/controllers/createCmde.js
+++ src/routes/commande/controllers/createCmde.js
@@ -1,10 +1,18 @@
 import Commande from "../../../models/commande";
+import DetailsCommande from "../../../models/detailsCommande";
 import _ from "lodash";
 
 export default async ({ bodymen: { body }}, res, next) => {
     try {
         const newCmde = await Commande.create({ userId: body.userId, status: 'EN COURS' });
         for (const event of body.evenements) {
+
+            await DetailsCommande.create({ 
+                commandeId: newCmde._id, 
+                evenementId: event._id,
+                horaire: event.horaire
+            });
+
             newCmde.evenements.push(event._id);
             await newCmde.save();
         }
